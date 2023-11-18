89f4d74569e58443a50d01add881724a1e1454b9
Pratik Nitin Gandole <69251362+PratikGandole@users.noreply.github.com>
2023-11-18 11:02:57 +0530

    Add files via upload

---
+++ new return train/script.js
@@ -0,0 +1,15 @@
+function openPopup() {
+  document.getElementById("popup").style.display = "block";
+}
+
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+}
+
+function openPopup2() {
+  document.getElementById("popup2").style.display = "block";
+}
+
+function closePopup2() {
+  document.getElementById("popup2").style.display = "none";
+}
